56f4c40fc3524adce44efbbd8a1b40c7d13e790f
kcgeo1234 <kcheng38@wisc.edu>
2023-02-21 23:15:44 -0600

    update MRT geojson

--- js/geojsonTutorial.js
+++ js/geojsonTutorial.js
@@ -1,5 +1,11 @@
 var map = L.map('map').setView([39.75621, -104.99404], 13); // setView([lat, long], Zoom)
 
+// var map = L.map('map', {
+//     center: [51.505, -0.09],
+//     zoom: 13
+// });
+
+
 // add tile layer
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
--- js/quickstartTutorial.js
+++ js/quickstartTutorial.js
@@ -1,11 +1,13 @@
 /* Example from Leaflet Quick Start Guide*/
 
-var map = L.map('map').setView([51.505, -0.09], 13); // setView([lat, long], Zoom)
+var map = L.map('map', {
+    zoom:13,
+    center:[51.505, -0.09]
+});// setView([lat, long], Zoom)
 
 // add tile layer
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
 	maxZoom: 20
 }).addTo(map);
 
